78361248463907282ce24a7185cbcc06feb2b8cd
tomdziwood <tomdziwood@gmail.com>
2018-12-30 00:00:53 +0100

    Postęp programu - są wyniki!
    Są wyniki, już jest coś, co powinno dać zaliczenie. To generalnie ujmując co się dzieje - wybierane są do weryfikacji tylko te fragmenty, dla których wartość amplitudy w dziedzinie częstotliwości w przedziale od 50Hz do 1000Hz jest większa niż 10% amplitudy sygnału dla dziedziny czasu. A potem jest szukany w tym przedziale częstotliwości pierwszy wzrost o conajmniej 20% maksymalnej amplitudzie w tym właśnie przedziale częstotliwości.

--- Sprawdzanie algorytmu.py
+++ Sprawdzanie algorytmu.py
@@ -1,4 +1,5 @@
 from __future__ import division
+import os
 import numpy as np
 from scipy import *
 import scipy.signal
@@ -24,6 +25,7 @@ def rysujKrok(czas, sygnal, freqs, sygnalHz, krok=200, tytul=''):
     ax.stem(freqs[::krok], sygnalHz[::krok], '-*')
 
     plt.show()
+    plt.close()
 
 
 
@@ -45,108 +47,216 @@ def rysuj(czas, sygnal, freqs, sygnalHz, tytul=''):
     ax.plot(freqs[:ilePunktow], sygnalHz[:ilePunktow], '-')
 
     plt.show()
+    #print("\tTrwa zapisywanie do pliku jpg...")
+    #fig.savefig('./wyniki/' + tytul[11:] + '.jpg')
+    plt.close()
+
+
+
+def rysujZle(czas, sygnal, freqs, sygnalHz, tytul=''):
+    fig = plt.figure(figsize=(20, 12), dpi=80)
+    fig.suptitle(tytul + ' (zle)', fontsize=15)
+
+    # print("\nTrwa rysowanie wykresu w dziedzinie czasu...")
+    ax = fig.add_subplot(211)
+    plt.xlabel("Czas [s]", fontsize=13)
+    plt.ylabel("Amplituda", fontsize=13)
+    ax.plot(czas, sygnal, '-')
+
+    # print("Trwa rysowanie wykresu w dziedzinie częstotliwości...")
+    ax = fig.add_subplot(212)
+    plt.xlabel("Częstotliwość [Hz]", fontsize=13)
+    plt.ylabel("Amplituda", fontsize=13)
+    ilePunktow = sum(freqs < 2000)
+    ax.plot(freqs[:ilePunktow], sygnalHz[:ilePunktow], '-')
+
+    #plt.show()
+    print("\tTrwa zapisywanie do pliku jpg...")
+    fig.savefig('./zle/' + tytul[11:] + '.jpg')
+    plt.close()
+
 
 
 
 
 def main():
-    tytulPliku = 'trainall/002_M.wav'
-    plik = scipy.io.wavfile.read(tytulPliku)
-    # print("type(plik) = " + str(type(plik)))
-    # print("len(plik) = " + str(len(plik)))
+    #tytulPliku = 'trainall/002_M.wav'
+    bledy = 0
+    dobre = 0
+    sciezkaZrodlowa = './trainall/'
+    tytulyPlikow = os.listdir(sciezkaZrodlowa)
+    #print(tytulyPlikow)
+    for iTytul in range(len(tytulyPlikow))[1:]:
+    #for iTytul in [2]:
+        #try:
+        tytulPliku = sciezkaZrodlowa + tytulyPlikow[iTytul]
+        #plik = scipy.io.wavfile.read(tytulPliku)
+        # print("Probuje otworzyć: " + tytulPliku)
+        plik = scipy.io.wavfile.read(tytulPliku)
+        # print("Ha!!! udało sie!!!: " + tytulPliku)
+        # print("type(plik) = " + str(type(plik)))
+        # print("len(plik) = " + str(len(plik)))
 
-    czestotliwoscProbkowania = plik[0]
-    # print("\ntype(czestotliwoscProbkowania) = " + str(type(czestotliwoscProbkowania)))
-    # #print("len(sygnal) = " + str(len(sygnal)))
-    # print("czestotliwoscProbkowania = " + str(czestotliwoscProbkowania))
+        czestotliwoscProbkowania = plik[0]
+        # print("\ntype(czestotliwoscProbkowania) = " + str(type(czestotliwoscProbkowania)))
+        # print("len(sygnal) = " + str(len(sygnal)))
+        # print("czestotliwoscProbkowania = " + str(czestotliwoscProbkowania))
 
-    sygnal = plik[1]
-    # print("\ntype(sygnal) = " + str(type(sygnal)))
-    print("len(sygnal) = " + str(len(sygnal)))
-    # print("sygnal.shape = " + str(sygnal.shape))
-    # print("sygnal = " + str(sygnal))
-    sygnal = np.mean(sygnal, axis=1)
-    dlugoscSygnalu = len(sygnal)
-    # print("\ntype(sygnal) = " + str(type(sygnal)))
-    # print("len(sygnal) = " + str(len(sygnal)))
-    # print("sygnal.shape = " + str(sygnal.shape))
-    # print("sygnal = " + str(sygnal))
-    absoluteSygnal = np.abs(sygnal)
-    mediana = np.median(absoluteSygnal)
-    print("mediana = " + str(mediana))
-    percentyl = np.percentile(absoluteSygnal, 95)
-    print("percentyl = " + str(percentyl))
+        sygnal = plik[1]
+        # print("\ntype(sygnal) = " + str(type(sygnal)))
+        # print("len(sygnal) = " + str(len(sygnal)))
+        # print("sygnal.shape = " + str(sygnal.shape))
+        # print("sygnal = " + str(sygnal))
+        if(len(sygnal.shape) > 1):
+            sygnal = np.mean(sygnal, axis=1)
+            # print("\ntype(sygnal) = " + str(type(sygnal)))
+            # print("len(sygnal) = " + str(len(sygnal)))
+            # print("sygnal.shape = " + str(sygnal.shape))
+            # print("sygnal = " + str(sygnal))
 
+        dlugoscSygnalu = len(sygnal)
+        absolutnySygnal = np.abs(sygnal)
+        mediana = np.median(absolutnySygnal)
+        # print("mediana = " + str(mediana))
+        percentyl = np.percentile(absolutnySygnal, 95)
+        # print("percentyl = " + str(percentyl))
 
-    iloscProbek = dlugoscSygnalu
-    czas = np.arange(0, iloscProbek)
-    czas = czas / czestotliwoscProbkowania
+        iloscProbek = dlugoscSygnalu
+        czas = np.arange(0, iloscProbek)
+        czas = czas / czestotliwoscProbkowania
 
+        """
+        # sygnal w dziedzinie czestotliwosci
+        print("Zaczynam fft...")
+        sygnalHz = fft(sygnal)
+        print("Skonczylem fft.")
 
-    # sygnal w dziedzinie czestotliwosci
-    sygnalHz = fft(sygnal)
-
-    # modul sygnalu
-    sygnalHz = abs(sygnalHz)
-
-    # normalizacja:
-    # dzielenie przez (ilość próbek/2)
-    sygnalHz /= (iloscProbek / 2)
-    # dzielenie wartosci dla 0Hz jeszce przez 2:
-    sygnalHz[0] /= 2
-
-    # ustawienie wartosci na osi x
-    freqs = np.arange(0, iloscProbek)
-    # print(freqs)
-    # print("min(freqs) = " + str(min(freqs)))
-    # print("max(freqs) = " + str(max(freqs)))
-    freqs = freqs / iloscProbek * czestotliwoscProbkowania
-    # print(freqs)
-    # print("min(freqs) = " + str(min(freqs)))
-    # print("max(freqs) = " + str(max(freqs)))
-
-    rysujKrok(czas, sygnal, freqs, sygnalHz)
-
-
-    flaga = absoluteSygnal >= percentyl
-    print(flaga)
-    print("sum(flaga) = " + str(sum(flaga)))
-
-    indeksy = np.where(flaga)[0]
-    print("indeksy = " + str(indeksy))
-
-    fragmentSzerokosc = int(czestotliwoscProbkowania / 10)
-    # fragmentSzerokosc = czestotliwoscProbkowania
-
-    oknoKaisera = kaiser(fragmentSzerokosc, 5)
-    # print("len(oknoKaisera) = " + str(len(oknoKaisera)))
-    # print("min(oknoKaisera) = " + str(min(oknoKaisera)))
-    # print("max(oknoKaisera) = " + str(max(oknoKaisera)))
-
-
-    i = 1
-    while((i <= 20) and (len(indeksy) > 0) and (indeksy[0] + fragmentSzerokosc < dlugoscSygnalu)):
-        indeksPoczatek = indeksy[0]
-        indeksKoniec = indeksPoczatek + fragmentSzerokosc
-
-        fragmentSygnal = sygnal[indeksPoczatek : indeksKoniec]
-        fragmentSygnal *= oknoKaisera
-        fragmentCzas = czas[indeksPoczatek : indeksKoniec]
-
-        iloscProbek = fragmentSzerokosc
-        sygnalHz = fft(fragmentSygnal)
+        # modul sygnalu
         sygnalHz = abs(sygnalHz)
+
+        # normalizacja:
+        # dzielenie przez (ilość próbek/2)
         sygnalHz /= (iloscProbek / 2)
+        # dzielenie wartosci dla 0Hz jeszce przez 2:
         sygnalHz[0] /= 2
+
+        # ustawienie wartosci na osi x
         freqs = np.arange(0, iloscProbek)
+        # print(freqs)
+        # print("min(freqs) = " + str(min(freqs)))
+        # print("max(freqs) = " + str(max(freqs)))
         freqs = freqs / iloscProbek * czestotliwoscProbkowania
+        # print(freqs)
+        # print("min(freqs) = " + str(min(freqs)))
+        # print("max(freqs) = " + str(max(freqs)))
 
-        tytulWykresu = tytulPliku + " - fragment " + str(i)
-        print("\t" + tytulWykresu + "\t[" + str(indeksPoczatek) + ", " + str(indeksKoniec) + "]")
-        rysuj(fragmentCzas, fragmentSygnal, freqs, sygnalHz, tytul=tytulWykresu)
+        #rysujKrok(czas, sygnal, freqs, sygnalHz)
+        """
 
-        indeksy = indeksy[indeksy >= indeksKoniec]
-        i += 1
+
+        flaga = absolutnySygnal >= percentyl
+        #print("sum(flaga) = " + str(sum(flaga)))
+
+        indeksy = np.where(flaga)[0]
+        #print("indeksy = " + str(indeksy))
+
+        fragmentSzerokosc = int(czestotliwoscProbkowania / 10)
+        # fragmentSzerokosc = czestotliwoscProbkowania
+
+        oknoKaisera = kaiser(fragmentSzerokosc, 5)
+        # print("len(oknoKaisera) = " + str(len(oknoKaisera)))
+        # print("min(oknoKaisera) = " + str(min(oknoKaisera)))
+        # print("max(oknoKaisera) = " + str(max(oknoKaisera)))
+
+
+        kobieta = 0
+        mezczyzna = 0
+        wynik = ''
+        i = 1
+        proba = 1
+        while((i <= 20) and (len(indeksy) > 0) and (indeksy[0] + fragmentSzerokosc < dlugoscSygnalu)):
+            #print("proba = " + str(proba))
+            indeksPoczatek = indeksy[0]
+            indeksKoniec = indeksPoczatek + fragmentSzerokosc
+
+            fragmentSygnal = np.zeros((fragmentSzerokosc))
+            fragmentSygnal += sygnal[indeksPoczatek : indeksKoniec]
+            fragmentSygnal *= oknoKaisera
+            fragmentCzas = czas[indeksPoczatek : indeksKoniec]
+
+            iloscProbek = fragmentSzerokosc
+            sygnalHz = fft(fragmentSygnal)
+            sygnalHz = abs(sygnalHz)
+            sygnalHz /= (iloscProbek / 2)
+            sygnalHz[0] /= 2
+            freqs = np.arange(0, iloscProbek)
+            freqs = freqs / iloscProbek * czestotliwoscProbkowania
+
+
+            #print("np.argmax(sygnalHz) = " + str(np.argmax(sygnalHz)))
+            #dominujacaCzestotliwosc = freqs[np.argmax(sygnalHz)]
+
+            malyIndeksPoczatek = sum(freqs < 50)
+            malyIndeksKoniec = sum(freqs < 1000)
+            malySygnalHz = sygnalHz[malyIndeksPoczatek : malyIndeksKoniec]
+            malyFreqs = freqs[malyIndeksPoczatek : malyIndeksKoniec]
+
+            maxFragmentSygnal = max(fragmentSygnal)
+            maxMalySygnalHz = max(malySygnalHz)
+
+
+            tytulWykresu = tytulPliku[:-4] + " - fragment " + str(proba)
+
+
+            if(maxMalySygnalHz / maxFragmentSygnal < 0.1):
+                i = i
+                #print("\t" + tytulWykresu + "\t[" + str(indeksPoczatek) + ", " + str(indeksKoniec) + ") - zle")
+                #rysujZle(fragmentCzas, fragmentSygnal, freqs, sygnalHz, tytul=tytulWykresu)
+            else:
+
+                print("\t" + tytulWykresu + "\t[" + str(indeksPoczatek) + ", " + str(indeksKoniec) + ") - dobre")
+                i += 1
+
+                j = 1
+                while((j < len(malySygnalHz)) and (malySygnalHz[j] - malySygnalHz[j - 1] < 0.2 * maxMalySygnalHz)):
+                    j += 1
+
+                if(j < len(malySygnalHz)):
+                    print(str(malyFreqs[j - 1]) + ': ' + str(malySygnalHz[j - 1]))
+                    print(str(malyFreqs[j]) + ': ' + str(malySygnalHz[j]))
+
+                if(j < len(malySygnalHz)):
+                    if(malyFreqs[j] < 150):
+                        mezczyzna += 1
+                    else:
+                        kobieta += 1
+
+                #rysuj(fragmentCzas, fragmentSygnal, freqs, sygnalHz, tytul=tytulWykresu)
+
+
+            indeksy = indeksy[indeksy >= indeksKoniec]
+            proba += 1
+
+        print('mezczyzna ' + str(mezczyzna) + ' - ' + str(kobieta) + ' kobieta')
+        if(mezczyzna > kobieta):
+            wynik = 'M'
+        else:
+            wynik = 'K'
+
+        print('wynik = ' + wynik)
+        print('tytulyPlikow[iTytul][-5] = ' + tytulyPlikow[iTytul][-5])
+        if(wynik == tytulyPlikow[iTytul][-5]):
+            dobre += 1
+        """
+        except ValueError:
+            print("Błąd dla pliku: " + str(tytulyPlikow[iTytul]))
+            bledy += 1
+        """
+
+        iTytul += 1
+
+    print("\nBłędnie otworzone pliki: " + str(bledy))
+    print(dobre,91,dobre/91)
 
 
 
